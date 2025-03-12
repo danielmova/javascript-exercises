fcd05bd7ccb566b23cf51a776b4e9bb9d6674183
danielmova <dataxf@gmail.com>
2025-03-12 10:17:25 -0500

    Solution to 02 String.concat is not a great option. I need to do an array and use push and join.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repetitionNumber) {
+    const finalString = [];
+    if(repetitionNumber < 0){
+        const ERROR = "ERROR"
+        return ERROR;
+    }
+    else {
+        for(let i = 0; i < repetitionNumber; i++){
+            finalString.push(stringToRepeat);
+        }
 
+        return finalString.join("");
+    }
 };
+
+console.log(repeatString("", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
