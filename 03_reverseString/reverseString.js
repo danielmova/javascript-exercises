ed5bc0634ad837d582b0e27b582868ad7418e621
danielmova <dataxf@gmail.com>
2025-03-12 10:54:26 -0500

    Solution 03 It was... easy? The hints on the README spoiled the fun from the excercise ajjaajja.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    arrayString = string.split("");
+    let finalString = "";
+    for(let i = arrayString.length; i != 0; i--){
+        finalString += arrayString[i - 1];
+    }
+    return finalString;
 };
+
+console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
