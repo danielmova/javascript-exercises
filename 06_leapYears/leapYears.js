7d0d347d76622d8a493ac2c2daec0e68fb6fc2b8
danielmova <dataxf@gmail.com>
2025-03-12 12:43:53 -0500

    Solution 06 I'm proud of my convoluted mess of ifs and elses... The solution sugested... Is just amazing............

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+
+    if(inputYear % 4 == 0){
+        if(inputYear % 100 == 0){
+            if(inputYear % 400 == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
 
 };
 
