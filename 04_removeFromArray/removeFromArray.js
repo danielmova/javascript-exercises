4f85dcb1d80e39897698943f66d16403be4a300f
danielmova <dataxf@gmail.com>
2025-03-12 11:44:16 -0500

    Solution 03 I don't know wht I do...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+function removeFromArray(arrayToClean, ...args) {
+    return arrayToClean.filter(x => !args.includes(x))
 };
+
+console.log(removeFromArray([1, 2, 3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
