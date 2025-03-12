2d01811c3cc82b98efb8d5c7f62da1d73e04682d
danielmova <dataxf@gmail.com>
2025-03-12 12:57:00 -0500

    Solution 07 The order of the excrcise are a little bit wird but... It's interesting.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(toCelsius) {
+  let celsiusFormula = (toCelsius - 32) * 5/9;
+  let totalCelsius = Math.round(celsiusFormula *10) / 10;
+  return totalCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(toFahrenheit) {
+  let fahrenheitFormula = (toFahrenheit * 1.8) + 32;
+  let totalFahrenheit = Math.round(fahrenheitFormula *10) / 10;
+  return totalFahrenheit;
 };
 
 // Do not edit below this line
